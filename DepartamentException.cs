2ac05953f5986f3c606067ccaafa15b32349ebeb
Carolina061511 <guerrerocarolina069@gmail.com>
2024-11-22 22:36:27 -0400

    Add files via upload

---
+++ DepartamentException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schooll.Infraestructura.Exceptions
+{
+    internal class DepartamentException
+    {
+    }
+}
